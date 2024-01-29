c6e97bf77d161b2a865295b3fafe2e651d7c1218
Nostea <kiwiuchi@googlemail.com>
2024-01-29 22:02:24 +0100

    added bucketlist task

---
+++ week7/29-01-2024-Value-ClassList-Onclick/tasks/classlist/classlist-1_1-main.js
@@ -0,0 +1,7 @@
+function assignClass() {
+  const list = document.querySelector("#headline");
+  list.classList.add("headline");
+
+  const list2 = document.querySelector("#item");
+  list2.classList.add("font");
+}
