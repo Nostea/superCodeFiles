3c23e430c448a78653ef2e52eacbdba9ec7ce668
Nostea <kiwiuchi@googlemail.com>
2024-01-29 22:16:33 +0100

    added bucketlist with style change and remove the style

---
+++ week7/29-01-2024-Value-ClassList-Onclick/tasks/classlist/classlist-2_1-main.js
@@ -0,0 +1,11 @@
+function styleHeadline() {
+  const addStyling = document.querySelector("#headline");
+
+  addStyling.classList.add("headline");
+}
+
+function resetHeadline() {
+  const resetStyling = document.querySelector("#headline");
+
+  resetStyling.classList.remove("headline");
+}
