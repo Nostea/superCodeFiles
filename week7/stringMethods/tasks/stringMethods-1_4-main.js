25e59dd7c992337a99ad075679fc51ed3dd89182
Nostea <kiwiuchi@googlemail.com>
2024-02-04 23:24:52 +0100

    updated with string method tasks

---
+++ week7/stringMethods/tasks/stringMethods-1_4-main.js
@@ -0,0 +1,12 @@
+const a = "Susi is going to codingschool";
+
+const text1 = a.slice(0, 4);
+const text2 = a.slice(5, 7);
+const text3 = a.slice(5, 17) + a.slice(23, 29);
+const text4 = a.slice(23, 29);
+const text5 = a.slice(0, 8) + a.slice(23, 29);
+console.log(text1);
+console.log(text2);
+console.log(text3);
+console.log(text4);
+console.log(text5);
