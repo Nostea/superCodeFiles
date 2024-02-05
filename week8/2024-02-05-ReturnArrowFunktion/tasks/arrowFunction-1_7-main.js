4f20a3f267e1cd4bb385f90b38ab380584d3af57
Nostea <kiwiuchi@googlemail.com>
2024-02-05 15:41:40 +0100

    updated with arrowFunction tasks

---
+++ week8/2024-02-05-ReturnArrowFunktion/tasks/arrowFunction-1_7-main.js
@@ -0,0 +1,5 @@
+const arrowFunction = (number1) => {
+  return console.log(number1 * 2);
+};
+
+arrowFunction(6);
---
+++ week8/2024-02-05-ReturnArrowFunktion/tasks/arrowFunction-1_8-main.js
@@ -0,0 +1,12 @@
+const ageCalc = (yearborn) => {
+  return console.log(yearborn);
+};
+
+let alterDiff;
+
+const ageComparison = (yearborn1, yearborn2) => {
+  return (alterDiff = yearborn1 - yearborn2);
+};
+
+ageCalc(1994);
+ageComparison(24, 26);
